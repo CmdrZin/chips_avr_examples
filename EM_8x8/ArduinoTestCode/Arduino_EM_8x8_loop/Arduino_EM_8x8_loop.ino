c161121abc039b74664b1838a6c05b3b45b64f42
Chip Perason <chip@gameactive.org>
2016-02-05 20:20:29 -0800

    Added API and license text
    Add the EM 8x8 API for commands and the MIT license to the code source
    files.

--- EM_8x8/ArduinoTestCode/Arduino_EM_8x8_loop/Arduino_EM_8x8_loop.ino
+++ EM_8x8/ArduinoTestCode/Arduino_EM_8x8_loop/Arduino_EM_8x8_loop.ino
@@ -1,4 +1,32 @@
-/* Arduino Emote Mod 8x8 Loop Animation Demo */
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Nels D. "Chip" Pearson (aka CmdrZin)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Arduino Emote Mod 8x8 Loop Animation Demo
+ * This code call a series of icons to cause the eye to roll around 
+ * near the outside edge.
+ */
 
 #include <Wire.h>
 
@@ -37,10 +65,10 @@ uint8_t outBuff[16];
 // List of icons to cycle through
 ANIM_ELEMENT animList[] = {
   { 0, 5000},      // normal eye..wait here for 5 seconds
-  {20,  200},     // close1
+  {20,  200},
   {13,  200},
   {14,  200},
-  {15,  200},      // closed
+  {15,  200},
   {16,  200},
   {17,  200},
   {18,  200},
--- EM_8x8/ArduinoTestCode/Arduino_EM_8x8_random/Arduino_EM_8x8_random.ino
+++ EM_8x8/ArduinoTestCode/Arduino_EM_8x8_random/Arduino_EM_8x8_random.ino
@@ -1,5 +1,32 @@
-/* Arduino Emote Mod 8x8 Load Icon Test - Random */
-/* Update the icon buffer each cycle to animate. */
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Nels D. "Chip" Pearson (aka CmdrZin)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Arduino Emote Mod 8x8 Load Icon Test - Random
+ * This code changes the icon patter in the buffer and loads the new patter 
+ * after a delay to case a random 8x8 display.
+ */
 
 #include <Wire.h>
 
